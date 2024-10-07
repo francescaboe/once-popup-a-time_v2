bc938a21d0fec72c0d8e6b324f43f04401469937
francescaboe <francesca.boe.ac@gmail.com>
2024-10-07 17:33:56 +0200

    lint

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { STORY } from 'utils/constants';
 import useKeyPress from 'hooks/useKeyPress';
 
 function App() {
-  const {t} = useTranslation();
+  const { t } = useTranslation();
   const [storyIndex, setStoryIndex] = React.useState(0);
 
   const handlePrevClick = () => {
@@ -22,7 +22,9 @@ function App() {
   return (
     <main className="container mx-auto flex flex-col items-center justify-center min-h-screen p-4">
       <div className="text-center mb-8">
-        <h2 className="text-2xl md:text-3xl lg:text-4xl font-bold mb-4">{t(STORY[storyIndex].title)}</h2>
+        <h2 className="text-2xl md:text-3xl lg:text-4xl font-bold mb-4">
+          {t(STORY[storyIndex].title)}
+        </h2>
         <p className="text-base md:text-lg lg:text-xl">{t(STORY[storyIndex].content)}</p>
       </div>
       <div className="w-64 h-64 border-8 border-amber-200 bg-amber-300 shadow-lg rounded-sm flex items-center justify-center text-gray-400 mb-8">
@@ -30,11 +32,15 @@ function App() {
       </div>
       <div className="flex space-x-4">
         <button
-          className="hidden md:block px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600" onClick={handlePrevClick}>
+          className="hidden md:block px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+          onClick={handlePrevClick}
+        >
           {t('prev')}
         </button>
         <button
-          className="hidden md:block px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600" onClick={handleNextClick}>
+          className="hidden md:block px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+          onClick={handleNextClick}
+        >
           {t('next')}
         </button>
       </div>
