7240c445519f58a2ac11428f8280a56123435704
francescaboe <francesca.boe.ac@gmail.com>
2024-10-09 22:58:29 +0200

    make content fit

--- src/App.tsx
+++ src/App.tsx
@@ -25,19 +25,19 @@ function App() {
   return (
     <main className="min-h-screen flex items-center bg-[radial-gradient(circle_at_center,_var(--tw-gradient-stops))] from-purple-900 via-black to-black">
       <div
-        className="container mx-auto flex flex-col items-center justify-center p-8 bg-amber-100 shadow-lg max-w-md text-amber-950 relative overflow-hidden"
+        className="container mx-auto flex flex-col items-center justify-center p-4 bg-amber-100 shadow-lg max-w-md md:max-w-lg text-amber-950 relative overflow-hidden"
         {...swipeHandlers}
       >
         {/* Gradient overlay top */}
         <div className="absolute top-0 left-0 w-full h-12 bg-gradient-to-b from-amber-200 to-transparent"></div>
         <div className=" text-center flex flex-col items-center mb-8">
-          <h2 className="text-2xl md:text-3xl lg:text-4xl font-bold font-gothic mt-4 mb-8">
+          <h2 className="text-2xl md:text-3xl lg:text-4xl font-bold font-gothic mt-4 mb-2">
             {t(STORY[storyIndex].title)}
           </h2>
-          <div className="w-64 h-64 border-8 border-amber-200 bg-amber-300 shadow-lg rounded-sm flex items-center justify-center text-gray-400 mb-2">
+          <div className="w-80 h-64 border-8 border-amber-200 bg-amber-300 shadow-lg rounded-sm flex items-center justify-center text-gray-400 mb-2">
             Space for picture!
           </div>
-          <p className="text-base md:text-lg lg:text-xl min-h-32 md:min-h-52 md:w-3/4 mt-8 overflow-y-auto font-fable">
+          <p className="text-base md:text-lg min-h-32 md:min-h-44 md:w-3/4 mt-4 overflow-y-auto font-fable">
             {('ontouchstart' in window || navigator.maxTouchPoints > 0) &&
             STORY[storyIndex].content_touchscreen
               ? t(STORY[storyIndex].content_touchscreen)
