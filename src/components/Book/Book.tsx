b0fc16b359285d6a12ffc7bd87fafd24d8a79e95
francescaboe <francesca.boe.ac@gmail.com>
2024-10-10 18:29:38 +0200

    add book component

---
+++ src/components/Book/Book.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Book = () => {
+  return (
+    <div className="book-container">
+      <div className="book">
+        <div className="flex flex-col items-center justify-center gap-2">
+          <h1 className="text-center font-bold font-gothic text-4xl">Welcome to my forest</h1>
+          <p>subtitle</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Book;
